40dc21c5d85713398c33c760f5aa6db446d6945c
Victor Messina <victormessina95@hotmail.com>
2017-11-07 16:06:30 -0200

    added new classes in models.py

--- Hotel/app/models.py
+++ Hotel/app/models.py
@@ -38,3 +38,33 @@ class UserInfo (models.Model):
     gender = models.CharField(max_length=9)
     birthday = models.DateField()
     fk_user_type = models.ForeignKey(UserType)
+
+class Place (models.Model):
+    place_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=10)
+    kitchen = models.BooleanField()
+    study_room = models.BooleanField()
+
+class RoomSize (models.Model):
+    room_size_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=6)
+
+class RoomInfo (models.Model):
+    room_info_id = models.AutoField(primary_key=True)
+    bathroom_inside = models.BooleanField()
+    bath = models.BooleanField()
+    double_bed = models.BooleanField()
+    study_table = models.BooleanField()
+    closet = models.BooleanField()
+    tv = models.BooleanField()
+    mirror = models.BooleanField()
+    nightstand = models.BooleanField()
+    fk_room_size = models.ForeignKey(RoomSize)
+
+class Room (models.Model):
+    room_id = models.AutoField(primary_key=True)
+    door_number = models.IntegerField()
+    availability = models.BooleanField()
+    share = models.BooleanField()
+    fk_room_info = models.ForeignKey(RoomInfo)
+    fk_place = models.ForeignKey(Place)
