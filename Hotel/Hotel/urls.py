ca4b0cc974786093e078eb62135b08b385fe8dc9
Victor Messina <victormessina95@hotmail.com>
2017-12-20 20:07:51 -0200

    create a new url for editprofile

--- Hotel/Hotel/urls.py
+++ Hotel/Hotel/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^signup$', app.views.sign_up, name='signup'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^profile$', app.views.profile, name='profile'),
+    url(r'^editprofile$', app.views.edit_profile, name='edit_profile'),
     url(r'^roomsdetails/(?P<room_id>\d+)$', app.views.rooms_details, name='rooms_details'),
     url(r'^$', app.views.login, name='login'),
     #url(r'^login/$',
