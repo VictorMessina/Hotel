c53321f48999af446501788879e92ab1aa6473e7
Victor Messina <victormessina95@hotmail.com>
2017-12-22 01:01:47 -0200

    finished methods for update user_info data

--- Hotel/app/postgres/user_info_functions.py
+++ Hotel/app/postgres/user_info_functions.py
@@ -10,3 +10,80 @@ def create_user_info(first_name, last_name, email, telephone, cellphone, gender,
     except UserInfo.DoesNotExist:
         print('user info not registered')
         return None
+
+
+def update_first_name(first_name, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.first_name = first_name
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("first name not updated")
+        return None
+
+
+def update_last_name(last_name, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.last_name = last_name
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("last name not updated")
+        return None
+
+
+def update_email(email, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.email = email
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("e-mail not updated")
+        return None
+
+
+def update_telephone(telephone, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.telephone = telephone
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("telephone not updated")
+        return None
+
+
+def update_cellphone(cellphone, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.cellphone = cellphone
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("cellphone not updated")
+        return None
+
+
+def update_gender(gender, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.gender = gender
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("gender not updated")
+        return None
+
+
+def update_birthday(birthday, user_id):
+    try:
+        user_info = UserInfo.objects.get(user_info_id = user_id)
+        user_info.birthday = birthday
+        user_info.save()
+        return True
+    except UserInfo.DoesNotExist:
+        print("birthday not updated")
+        return None
